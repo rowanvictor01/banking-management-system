4e775115def232cc9a51b2c32f4db6fbd15a9e69
Rowan Victor <209355544+rowanvictor01@users.noreply.github.com>
2025-12-18 22:21:53 +0800

    Implement account creation function

--- main.c
+++ main.c
@@ -1,92 +1,150 @@
-#include <stdlib.h>
+/*
+ * Create an array of function pointers and activate a function depending on
+ * the current session
+ */
+
+#include <stdbool.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <strings.h>
-#include <stdbool.h>
+#include <time.h>
 
-
-typedef enum {
-  REGISTER = 0,
-  LOGIN = 1,
-  SIGNED_IN = 2
-} Session;
-
+typedef enum { REGISTER = 0, LOGIN = 1, SIGNED_IN = 2 } session_e;
 
 void display_menu();
 void create_account();
 void clear_input_buffer();
-Session login_or_register();
-bool check_input_overflow(char*);
-
+session_e login_or_register();
+bool check_input_overflow(char *);
+void get_user_input(char*, int);
+void get_current_local_time(char*, int);
 
 int main() {
-  /*
-  * Create an array of function pointers and activate a function depending on the current session
-  */
-  Session current_session = login_or_register();
+    // Seed random num generator using current time
+    srand(time(NULL));
 
-  return 0;
+    session_e current_session = login_or_register();
+
+    // call function depending on session
+    switch(current_session) {
+        case REGISTER:
+                create_account();
+                break;
+        case LOGIN:
+                // code
+                break;
+    }
+
+    return 0;
 }
 
-void display_menu() {
-}
+void display_menu() {}
 
 void create_account() {
+    // Later write a function to avoid duplicating exising acc num
+    long new_account_num = (rand() % 9999999) + 1000000;
+
+    // Refactor later to dynamically allocate memory instead of setting an arbitrary size
+    char user_name[99];
+    float deposit;
+    char deposit_buf[14];
+
+    printf("\nEnter your full name\n>> ");
+    get_user_input(user_name, sizeof(user_name));
+
+    printf("Enter your initial deposit amount\n>> ");
+    get_user_input(deposit_buf, sizeof(deposit_buf));
+    sscanf(deposit_buf, "%f", &deposit);
+
+    printf("\nName: %s\n", user_name);
+    printf("Balance: %.3f\n", deposit);
+
+    char acc_creation_time[80];
+    get_current_local_time(acc_creation_time, sizeof(acc_creation_time));
+    printf("Account Created @ %s\n", acc_creation_time);
 }
 
-Session login_or_register() {
-  printf("\nWelcome to the Banking Application!\n\nWould you like to login or register?\n>> ");
+session_e login_or_register() {
+    printf("\nWelcome to the Banking Application!\n\nWould you like to login "
+           "or register?\n>> ");
 
-  char user_response[10];
-  char input_buffer[10];
-  Session chosen_session;
-  bool has_valid_answer = false;
-  bool is_input_overflowing = false;
+    char user_response[10];
+    session_e chosen_session;
+    bool has_valid_answer = false;
 
-  do {
+    do {
 
-    if(fgets(input_buffer, sizeof(input_buffer), stdin) != NULL) {
+            get_user_input(user_response, sizeof(user_response));
 
-      is_input_overflowing = check_input_overflow(input_buffer);
+            if (strcasecmp(user_response, "login") == 0) {
+                chosen_session = LOGIN;
+                has_valid_answer = true;
+            } else if (strcasecmp(user_response, "register") == 0) {
+                chosen_session = REGISTER;
+                has_valid_answer = true;
+            } else {
+                printf("Invalid Input. Please enter with either [login] or "
+                       "[register] only\n>> ");
+            }
 
-      if(is_input_overflowing) {
-        clear_input_buffer();
-      }
+    } while (!has_valid_answer);
 
-      sscanf(input_buffer, "%s", user_response);
-      strcpy(input_buffer, "");
-      
-      if(strcasecmp(user_response, "login") == 0) {
-        chosen_session = LOGIN;
-        has_valid_answer = true;
-      }
-      else if(strcasecmp(user_response, "register") == 0) {
-        chosen_session = REGISTER;
-        has_valid_answer = true;
-      }
-      else {
-        printf("Invalid Input. Please enter with either [login] or [register] only\n>> ");
-      }
+    return chosen_session;
+}
+
+void get_user_input(char* input_var, int size) {
+
+    bool is_input_overflowing = false;
+    char input_buf[size];
+
+    if (fgets(input_buf, sizeof(input_buf), stdin) != NULL) {
+
+        is_input_overflowing = check_input_overflow(input_buf);
+
+        if (is_input_overflowing) {
+            clear_input_buffer();
+        }
+        else {
+            size_t len = strlen(input_buf);
+            input_buf[len - 1] = '\0';
+            len--;
+        }
+
+        strcpy(input_var, input_buf);
+
     }
 
-  } while(!has_valid_answer);
-
-  return chosen_session;
 }
+
 
 void clear_input_buffer() {
-  int c;
-  while((c = getchar()) != '\n' && c != EOF) {
-    ;
-  }
+    int c;
+    while ((c = getchar()) != '\n' && c != EOF) {
+        ;
+    }
 }
 
-bool check_input_overflow(char* input) {
-  while(*input != '\0') {
-    if(*input == '\n') {
-      return false;
+bool check_input_overflow(char *input) {
+    while (*input != '\0') {
+        if (*input == '\n') {
+            return false;
+        }
+        input++;
     }
-    input++;
-  }
-  return true;
+    return true;
+}
+
+void get_current_local_time(char* time_var, int size) {
+    time_t raw_time;
+    struct tm* local_time_info_s;
+
+    // Get current calendar time in seconds
+    time(&raw_time);
+
+    // Break down time into components like hours and minutes and store into a struct
+    local_time_info_s = localtime(&raw_time);
+
+    // Format time into a custom string
+    strftime(time_var, size, "%m-%d, %H:%M %Y", local_time_info_s);
 }
